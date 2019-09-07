4a7ba11ff9ccf686df399caee4983f010180195f
joiceMarek <joicemarek@gmail.com>
2019-09-07 19:22:05 -0300

    update cos() test

--- software/tests/math/cos/test.c
+++ software/tests/math/cos/test.c
@@ -1,22 +1,31 @@
 #include <hf-risc.h>
 #include <hf-unit.h>
 
-union ufloat{
-	float f;
-	unsigned u;
-};
+const float pi = 3.14159265359;
 
 // function under test 
 float cos(float arg);
 
 // list of individual tests
-void cos1_test();
-void test();
+void cos_0_degree_test();
+void cos_1_degree_test();
+void cos_90_degree_test();
+void cos_89_degree_test();
+void cos_91_degree_test();
+void cos_180_degree_test();
+void cos_300_degree_test();
 
 // main test
 void hfunit_run_tests(){
-	cos1_test();//0
-	cos_test2();//360
+	cos_0_degree_test();
+	cos_1_degree_test();
+	cos_90_degree_test();
+	cos_89_degree_test();
+	cos_91_degree_test();
+	cos_180_degree_test();
+	cos_300_degree_test();
+	//cos1_test();//0
+/*	cos_test2();//360
 	cos_test3();//30
 	cos_test4();//45
 	cos_test5();//60
@@ -32,28 +41,153 @@ void hfunit_run_tests(){
 	cos_test15();//300
 	cos_test16();//315
 	cos_test17();//330
-//	test();
-	//cos1_test();
+*/
 }
 
 // place here a nice description for each test
-void cos1_test(){
-	//float pi = 4 * atan(1);
-	float pi = 3.14159265359;	
+void cos_0_degree_test(){
 	float degree = 0.0;	
 	float radians;
-
 	float expected = 1.0;
+	float output;
 	char buf[30];
-	radians = 0.0f;// degree * pi / 180;
-	printf("\nAngle is %d degrees.\n", (int)degree);
-	/*cosine*/
+	radians = degree * pi / 180;
 	ftoa(radians, buf, 6);
-	printf("rad: %s\n", buf);
-	ftoa(cos(radians), buf, 6);
-	printf("cos: %s\n", buf);
-	hfunit_comp_float(cos(radians),expected, "cos(0.0)");
+	printf("\nAngle is %d degrees (%s rad).\n", (int)degree, buf);
+	output = cos(radians);
+	printf(" ");
+	printBits(sizeof(float),&expected);
+	printf("=");
+	printBits(sizeof(float),&output);
+	ftoa(expected, buf, 6);
+	printf("expected: %s\n", buf);
+	ftoa(output, buf, 6);
+	printf("output: %s\n", buf);
+	hfunit_comp_float(output,expected, "cos(0)");
 }
+
+void cos_1_degree_test(){
+	float degree = 1.0;	
+	float radians;
+	float expected = 0.999848;
+	float output;
+	char buf[30];
+	radians = degree * pi / 180;
+	ftoa(radians, buf, 6);
+	printf("\nAngle is %d degrees (%s rad).\n", (int)degree, buf);
+	output = cos(radians);
+	printf(" ");
+	printBits(sizeof(float),&expected);
+	printf("=");
+	printBits(sizeof(float),&output);
+	ftoa(expected, buf, 6);
+	printf("expected: %s\n", buf);
+	ftoa(output, buf, 6);
+	printf("output: %s\n", buf);
+	hfunit_comp_float(output,expected, "cos(1)");
+}
+void cos_90_degree_test(){
+	float degree = 90.0;	
+	float radians;
+	float expected = 0.0;
+	float output;
+	char buf[30];
+	radians = degree * pi / 180;
+	ftoa(radians, buf, 6);
+	printf("\nAngle is %d degrees (%s rad).\n", (int)degree, buf);
+	output = cos(radians);
+	printf(" ");
+	printBits(sizeof(float),&expected);
+	printf("=");
+	printBits(sizeof(float),&output);
+	ftoa(expected, buf, 6);
+	printf("expected: %s\n", buf);
+	ftoa(output, buf, 6);
+	printf("output: %s\n", buf);
+	hfunit_comp_float(output,expected, "cos(90)");
+}
+void cos_89_degree_test(){
+	float degree = 89.0;	
+	float radians;
+	float expected = 0.017452;
+	float output;
+	char buf[30];
+	radians = degree * pi / 180;
+	ftoa(radians, buf, 6);
+	printf("\nAngle is %d degrees (%s rad).\n", (int)degree, buf);
+	output = cos(radians);
+	printf(" ");
+	printBits(sizeof(float),&expected);
+	printf("=");
+	printBits(sizeof(float),&output);
+	ftoa(expected, buf, 6);
+	printf("expected: %s\n", buf);
+	ftoa(output, buf, 6);
+	printf("output: %s\n", buf);
+	hfunit_comp_float(output,expected, "cos(89)");
+}
+void cos_91_degree_test(){
+	float degree = 91.0;	
+	float radians;
+	float expected = -0.017452;
+	float output;
+	char buf[30];
+	radians = degree * pi / 180;
+	ftoa(radians, buf, 6);
+	printf("\nAngle is %d degrees (%s rad).\n", (int)degree, buf);
+	output = cos(radians);
+	printf(" ");
+	printBits(sizeof(float),&expected);
+	printf("=");
+	printBits(sizeof(float),&output);
+	ftoa(expected, buf, 6);
+	printf("expected: %s\n", buf);
+	ftoa(output, buf, 6);
+	printf("output: %s\n", buf);
+	hfunit_comp_float(output,expected, "cos(91)");
+}
+void cos_180_degree_test(){
+	float degree = 180.0;	
+	float radians;
+	float expected = -1;
+	float output;
+	char buf[30];
+	radians = degree * pi / 180;
+	ftoa(radians, buf, 6);
+	printf("\nAngle is %d degrees (%s rad).\n", (int)degree, buf);
+	output = cos(radians);
+	printf(" ");
+	printBits(sizeof(float),&expected);
+	printf("=");
+	printBits(sizeof(float),&output);
+	ftoa(expected, buf, 6);
+	printf("expected: %s\n", buf);
+	ftoa(output, buf, 6);
+	printf("output: %s\n", buf);
+	hfunit_comp_float(output,expected, "cos(180)");
+}
+
+void cos_300_degree_test(){
+	float degree = 300.0;	
+	float radians;
+	float expected = 0.5;
+	float output;
+	char buf[30];
+	radians = degree * pi / 180;
+	ftoa(radians, buf, 6);
+	printf("\nAngle is %d degrees (%s rad).\n", (int)degree, buf);
+	output = cos(radians);
+	printf(" ");
+	printBits(sizeof(float),&expected);
+	printf("=");
+	printBits(sizeof(float),&output);
+	ftoa(expected, buf, 6);
+	printf("expected: %s\n", buf);
+	ftoa(output, buf, 6);
+	printf("output: %s\n", buf);
+	hfunit_comp_float(output,expected, "cos(300)");
+}
+
 void cos_test2(){
 	float pi = 4 * atan(1);
 	//float degrees[] = {0.0, 30.0, 45.0, 60.0, 90.0, 120.0, 135.0, 150.0, 180.0, 210.0, 225.0, 240.0, 270.0, 300.0, 315.0, 330.0, 360.0};
@@ -272,6 +406,8 @@ void cos_test6(){
 	radians = 5.23599f;// degree * pi / 180;
 	printf("\nAngle is %d degrees.\n", (int)degree);
 	/*cosine*/
+	ftoa(radians, buf, 6);
+	printf("rad: %s\n", buf);
 	ftoa(cos(radians), buf, 6);
 	printf("cos: %s\n", buf);
 	hfunit_comp_float(cos(radians),expected, "cos(300.0)");
