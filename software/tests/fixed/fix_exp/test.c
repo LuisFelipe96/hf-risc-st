41cef47ef5538197addde0fecf2b81b64a1f2cff
joiceMarek <joicemarek@gmail.com>
2019-09-13 16:41:27 -0300

    update

--- software/tests/fixed/fix_exp/test.c
+++ software/tests/fixed/fix_exp/test.c
@@ -8,7 +8,6 @@ typedef fixed_t typ_var;
 fixed_t fix_exp(fixed_t arg);
 
 // list of individual tests
-int hfunit_comp_fixed(fixed_t f1, fixed_t f2, char *menssage);
 void fix_exp_test();
 
 // main test
@@ -23,31 +22,6 @@ void hfunit_run_tests(){
 	fix_exp_testMin();
 }
 
-int hfunit_comp_fixed(fixed_t f1, fixed_t f2, char *menssage){
-	test_counter++;
-	fixed_t precision = fix_val(0.1);
-//	if (!(((f1 - precision) < f2) && ((f1 + precision) > f2)))
-//(fix_exp(fix_val(1.0)) <= fix_val(2.718261)+fix_exp(0.001)) && (fix_exp(entrada) >= fix_val(2.718261)-fix_val(0.001))
-	
-	fix_print(f1);
-	printf(" = ");
-	fix_print(f2);
-	printf("\n");
-	if ((f2-f1 >= fix_val(0.0)-precision) && (f2-f1 <= fix_val(0.0)+precision))
-	{
-		printf("test %d) \'%s\' passed\n",test_counter, menssage);
-		return 0;
-	}else{
-		
-		failed_tests++;
-		printf("test %d) \'%s\' failed\n",test_counter, menssage);
-		return 1;
-	}
-
-
-
-}
-// place here a nice description for each test
 /*
 |Entrada | Classes de Equivalência Válidas| Classes de Equivalência Inválidas |
 |--------|--------------------------------|-----------------------------------|
