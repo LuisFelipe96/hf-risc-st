3e6770f83cacfab5c68a633204ae1de2cb05ba74
LuisFelipe96 <luis.castro@acad.pucrs.br>
2019-09-29 16:46:18 -0300

    test.sh

--- test.sh
+++ test.sh
@@ -2,17 +2,24 @@
 
 cd ./software/tests/math/cos
 make test
+cd ..
 cd ./software/tests/math/sin
 make test
+cd ../..
 cd ./software/tests/libc/memcpy
 make test
+cd ..
 cd ./software/tests/libc/strcat
 make test
+cd ../..
 cd ./software/tests/fixed/fix_exp
 make test
+cd ..
 cd ./software/tests/fixed/fix_sqrt
 make test
+cd ../..
 cd ./software/tests/matrix/copy
 make test
+cd ..
 cd ./software/tests/matrix/transposed
 make test
