4b55881ea2c49181c8d536243203d7a1a5091091
LuisFelipe96 <luis.castro@acad.pucrs.br>
2019-09-30 13:49:18 -0300

    instal

--- instal.sh
+++ instal.sh
@@ -3,6 +3,6 @@
 
 gdown 'https://drive.google.com/uc?id=1ntx6Mpor-GWRl3CSiAQ47H8RQOKYeoWJ' 
 tar xvzf riscv-gcc.tar.gz --strip-components 2
-echo "export PATH=/home/travis/build/LuisFelipe96/riscv-elf/gcc-8.1.0/bin:$PATH" >> ~/.bashrc
+echo "export PATH=/home/travis/build/LuisFelipe96/soft64/util/riscv-elf/gcc-8.1.0/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
 riscv32-unknown-elf-cpp --version
