0a7837e403a70d392c612e9462c4c1571f6ae03d
joiceMarek <joicemarek@gmail.com>
2019-09-30 17:56:12 -0300

    add gprof test

--- test.sh
+++ test.sh
@@ -2,56 +2,86 @@
 
 cd ./software/tests/math/cos
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
+gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
 wait
 
 cd ..
 cd sin
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
+gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
 wait
 
 cd ../..
 cd ./libc/memcpy
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
+gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
 wait
 
 cd ..
 cd strcat
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
+gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
 wait
 
 cd ../..
 cd fixed/fix_exp
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
 gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
 wait
 
 cd ..
 cd ./fix_sqrt
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
+gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
 wait
 
 cd ../..
 cd ./matrix/copy
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
+gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
 wait
 
 cd ..
 cd transposed
 make test
-cppcheck test.c > out.log
+cppcheck test.c > cppcheck.log
 gcov hf_riscv_sim.c >gcov.log
-
+gprof ./hf_riscv_sim gmon.out >gprof.log
+head cppcheck.log -n 200
+head gcov.log -n 200
+head gprof.log -n 200
