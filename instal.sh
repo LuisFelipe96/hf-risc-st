9b9fc58b58177973f6b402ad1c74842f9252870c
joiceMarek <joicemarek@gmail.com>
2019-09-30 16:16:09 -0300

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -7,13 +7,13 @@ cd RISCV
 gdown 'https://drive.google.com/uc?id=1ntx6Mpor-GWRl3CSiAQ47H8RQOKYeoWJ' 
 tar xvzf riscv-gcc.tar.gz --strip-components 2
 #ls
-cd riscv-elf/gcc-8.1.0/bin
-pwd
+#cd riscv-elf/gcc-8.1.0/bin
+#pwd
 #ls -l
 
-echo "export PATH=/home/travis/build/RISCV/riscv-elf/gcc-8.1.0/bin:$PATH" >> ~/.bashrc
+#echo "export PATH=/home/travis/build/RISCV/riscv-elf/gcc-8.1.0/bin:$PATH" >> ~/.bashrc
 #cat ~/.bashrc
-sudo source ~/.bashrc
+#source ~/.bashrc
 #cd ../../..
 riscv32-unknown-elf-cpp --version
 
