4c7d2306e6c83d74b4e4fa0f26023f78f3c24c3e
LuisFelipe96 <luis.castro@acad.pucrs.br>
2019-09-29 22:56:28 -0300

    gcov ok

--- instal.sh
+++ instal.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+wget 
+tar xvzf riscv-gcc.tar.gz --strip-components 2
 
+echo "export PATH=/home/travis/build/LuisFelipe96/riscv-elf/gcc-8.1.0/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
--- test.sh
+++ test.sh
@@ -2,47 +2,55 @@
 
 cd ./software/tests/math/cos
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 wait
 cd ../..
 cd ./math/sin
 #/software/tests/math
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 wait
 cd ../..
 cd ./libc/memcpy
 #/software/tests/
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 wait
 cd ../..
 cd ./libc/strcat
 #/software/tests/libc
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 wait
 cd ../..
 cd ./fixed/fix_exp
 #/software/tests
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 wait
 cd ..
 cd ./fix_sqrt
 #cd ./software/tests/fixed/fix_sqrt
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 wait
 cd ../..
 cd ./matrix/copy
 #/software/tests
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 wait
 cd ..
 cd ./transposed
 #software/tests/matrix/
 make test
-cppcheck test.c > out.txt
+cppcheck test.c > out.log
+gcov hf_riscv_sim.c >gcov.log
 
