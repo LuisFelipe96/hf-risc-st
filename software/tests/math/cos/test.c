a05dc09ca2682b043c92a8e8a083660660df88c2
LuisFelipe96 <luis.castro@acad.pucrs.br>
2019-10-02 00:21:18 -0300

    Update test.c

--- software/tests/math/cos/test.c
+++ software/tests/math/cos/test.c
@@ -140,7 +140,7 @@ void cos_zero_test_2(){
 	//printf("expected: %s\n", buf);
 	ftoa(output, buf, digits);
 	//printf("output: %s\n", buf);
-	hfunit_comp_float(output,expected, "cos((+)quase zero)",1);
+	hfunit_comp_float(output,expected, "cos((+)quase zero)",0);
 }
 
 /*
